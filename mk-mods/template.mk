6fbb361d29e5bafc313020c2090c7ba179f49a6f
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-04-22 12:39:25 +0800

    Update the project structure.

---
+++ mk-mods/template.mk
@@ -0,0 +1,32 @@
+YML_FILES  = docker-compose.yml
+YML_FILES += $(wildcard */docker-compose.yml)
+
+ifndef COMPOSE_BIN
+COMPOSE_BIN   = docker-compose
+endif
+COMPOSE_ARGS += $(foreach yml,$(YML_FILES),-f $(yml))
+
+
+.PHONY: all clean purge
+.PHONY: up config down prepare
+
+all: up
+
+clean: down
+
+include ../mk-mods/version.mk    # version checking support
+
+up: version prepare $(YML_FILES)
+	$(COMPOSE_BIN) $(COMPOSE_ARGS) up
+
+config: version $(YML_FILES)
+	$(COMPOSE_BIN) $(COMPOSE_ARGS) config
+
+down: version $(YML_FILES)
+	-$(COMPOSE_BIN) $(COMPOSE_ARGS) down -v
+
+purge: clean
+	-sudo rm -rvf $(DATA_FOLDER)
+	-sudo rm -rvf $(TMP_FOLDER)
+
+prepare:
