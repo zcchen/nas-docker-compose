11932ebb0733f5b7c041f6af766f2f96077f786c
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-07-20 11:31:45 +0800

    Update the entry script for acme & dnsmasq service.

--- local/nginx/acme-entry.sh
+++ local/nginx/acme-entry.sh
@@ -3,18 +3,20 @@
 CERT_KEY_PATH=/acme.sh/cert.key
 CERT_PEM_PATH=/acme.sh/cert.pem
 
+if [ ! -f ${CERT_KEY_PATH} || ! -f ${CERT_PEM_PATH} ]; then
+  /bin/sh /usr/local/bin/acme.sh \
+    --key-file ${CERT_KEY_PATH} \
+    --fullchain-file ${CERT_PEM_PATH} \
+    --issue $PARAMS \
+    -d $LOCAL_DOMAIN_NAME \
+    $@
+fi
+
 if [ -f ${CERT_KEY_PATH} && -f ${CERT_PEM_PATH} ]; then
   /bin/sh /usr/local/bin/acme.sh \
     --key-file ${CERT_KEY_PATH} \
     --fullchain-file ${CERT_PEM_PATH} \
     --cron $PARAMS \
-    -d $LOCAL_DOMAIN_NAME \
-    $@
-else
-  /bin/sh /usr/local/bin/acme.sh \
-    --key-file ${CERT_KEY_PATH} \
-    --fullchain-file ${CERT_PEM_PATH} \
-    --issue $PARAMS \
     -d $LOCAL_DOMAIN_NAME \
     $@
 fi
