a3f9b27587155a83d5d11bce0458ee7f3b54ba7d
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-07-20 11:31:45 +0800

    Update the entry script for acme & dnsmasq service.

--- local/dnsmasq/dnsmasq-start.sh
+++ local/dnsmasq/dnsmasq-start.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env sh
 
-CMD="watch ls -Alh"
+CMD="ping www.baidu.com"
 CHECK_FILE=/tmp/abc.txt
 
 
 RUNNING=1;
 
+is_files_ready() {
+    if [ -f ${CHECK_FILE} ]; then
+        return 0
+    else
+        echo "Files are NOT ready, waiting..."
+        return 1
+    fi
+}
+
 main() {
     # very big pid, which is small chance to duplicate
-    pid=9999999999999
+    pid=99999999999999
 
     while [ ${RUNNING} -ne 0 ]; do
-        if [ ! -f ${CHECK_FILE} ]; then
-            kill -0 $pid 2>&1 1>/dev/null   # check if $pid is running
+        is_files_ready
+        files_are_ready=$?
+        if [ ${files_are_ready} -ne 0 ]; then
+            kill -0 $pid 1>/dev/null 2>/dev/null   # check if $pid is running
             if [ $? -eq 0 ]; then
                 echo "pid <$pid> exists, kill it."
                 kill $pid   # pid exists, kill it.
             fi
         else
-            kill -0 $pid 2>&1 1>/dev/null   # check if $pid is running
+            kill -0 $pid 1>/dev/null 2>/dev/null   # check if $pid is running
             if [ $? -ne 0 ]; then
                 echo "command does not start, run it."
-                $(echo ${CMD}) &    # $pid not exists, run it.
+                ${CMD} &    # $pid not exists, run it.
                 pid=$!
                 echo "command starts correctly, pid is <$pid>."
             fi
@@ -35,5 +46,9 @@ on_exit() {
     RUNNING=0;
 }
 
-trap on_exit ERR
+trap on_exit 0
+trap on_exit 1
+trap on_exit 2
+trap on_exit 3
+trap on_exit 15
 main
