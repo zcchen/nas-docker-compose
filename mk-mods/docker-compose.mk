e03105570f63ff2457cf4b2b1fcfad55665d6fec
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-07-26 11:26:34 +0800

    Update the makefile components.

---
+++ mk-mods/docker-compose.mk
@@ -0,0 +1,41 @@
+YML_FILES  = docker-compose.yml
+YML_FILES += $(wildcard */docker-compose.yml)
+
+ifndef COMPOSE_BIN
+COMPOSE_BIN   = docker-compose
+endif
+COMPOSE_ARGS += $(foreach yml,$(YML_FILES),-f $(yml))
+
+COMPOSE_VER_REQ   = 1.24.0
+COMPOSE_VER       = $(word 3, $(shell $(COMPOSE_BIN) -v))
+COMPOSE_VER_MAJOR = $(word 1, $(subst ., ,$(COMPOSE_VER)))
+COMPOSE_VER_MINOR = $(word 2, $(subst ., ,$(COMPOSE_VER)))
+COMPOSE_VER_REQ_MAJOR = $(word 1, $(subst ., ,$(COMPOSE_VER_REQ)))
+COMPOSE_VER_REQ_MINOR = $(word 2, $(subst ., ,$(COMPOSE_VER_REQ)))
+
+.PHONY: docker-compose/up docker-compose/config docker-compose/down docker-compose/prepare
+.PHONY: docker-compose/version
+
+docker-compose/up: docker-compose/version docker-compose/prepare $(YML_FILES)
+	$(COMPOSE_BIN) $(COMPOSE_ARGS) up --remove-orphans
+
+docker-compose/config: docker-compose/version $(YML_FILES)
+	$(COMPOSE_BIN) $(COMPOSE_ARGS) config
+
+docker-compose/down: docker-compose/version $(YML_FILES)
+	-$(COMPOSE_BIN) $(COMPOSE_ARGS) down --volumes --remove-orphans
+
+docker-compose/purge: docker-compose/down
+	-sudo rm -rvf $(DATA_FOLDER)
+	-sudo rm -rvf $(TMP_FOLDER)
+
+docker-compose/prepare:
+
+docker-compose/version:
+	@echo "-------- docker compose version ----------"
+	@echo "Version $(COMPOSE_VER)"
+	@echo "Reqired >= $(COMPOSE_VER_REQ)"
+	@echo "------------------------------------------"
+	@[ $(COMPOSE_VER_MAJOR) -eq $(COMPOSE_VER_REQ_MAJOR) ] || return 1
+	@[ $(COMPOSE_VER_MINOR) -gt $(COMPOSE_VER_REQ_MINOR) ] || return 1
+
--- mk-mods/systemd.mk
+++ mk-mods/systemd.mk
@@ -1,7 +1,29 @@
+current_dir := $(notdir $(CURDIR))
+
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
-current_dir := $(notdir $(patsubst %/,%,$(dir $(mkfile_path))))
+project_dir := $(dir $(mkfile_path))
+systemd_dir := $(abspath $(project_dir)/systemd/)
 
-.PHONY: systemd-service
+service_template = $(abspath $(systemd_dir)/nas.service)
+service_filename = nas-$(current_dir).service
 
-systemd-service:
-	echo "current_dir: $(current_dir)"
+
+$(service_filename):
+	sed $(service_template) \
+		-e 's#@MOD_NAME@#$(current_dir)#g' \
+		-e 's#@USER@#$(shell id -un)#g' \
+		-e 's#@ABSOLUTED_PATH@#$(CURDIR)#g' > $@
+
+.PHONY: $(service_filename)/install
+$(service_filename)/install: $(service_filename)
+	install -p -D -m 0644 $< /etc/systemd/system/$<
+	systemctl daemon-reload
+
+.PHONY: $(service_filename)/clean
+$(service_filename)/clean:
+	-rm $(service_filename)
+
+.PHONY: $(service_filename)/uninstall
+$(service_filename)/uninstall:
+	-rm /etc/systemd/system/$(service_filename)
+	systemctl daemon-reload
