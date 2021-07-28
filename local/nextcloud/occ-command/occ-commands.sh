30ba08adb9b2d20b7defeea8039efb049c3e8566
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-07-28 13:47:24 +0800

    Try to add the nextcloud occ script interface.

---
+++ local/nextcloud/occ-command/occ-commands.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Please fill this script the occ commands as you want.
+# ref: https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html
+
+# Example, just echo the current executing user ID to ensure the privileges.
+echo "User ID: <$(id)>"
