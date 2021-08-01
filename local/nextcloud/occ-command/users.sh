1dd646e814d9bd104056860a3a35045e4c3e9602
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-08-02 00:40:29 +0800

    Able to create the auto shared folders. Privileges & groups are TBD.

--- local/nextcloud/occ-command/users.sh
+++ local/nextcloud/occ-command/users.sh
@@ -5,8 +5,10 @@ occ_cmd="php /var/www/html/occ"
 public_username=public
 public_password=$(cat /run/secrets/nextcloud-public-password)
 public_homedir=/var/www/html/data/${public_username}
+users_groupname=users
 
 # user: public, password: publicPassword, group public
+${occ_cmd} group:add ${users_groupname}
 ${occ_cmd} group:add ${public_username}
 OC_PASS=${public_password} ${occ_cmd} user:add ${public_username} \
         --no-interaction --password-from-env --group="${public_username}"
@@ -20,10 +22,19 @@ fi
 for k in "Public" "Downloads" "Movies" "Pictures" "Softwares"; do
     curl -u ${public_username}:${public_password} -i -L -X MKCOL \
         http://nextcloud-nginx/remote.php/dav/files/${public_username}/${k}
+
     if [ $? -ne 0 ]; then
         exit 1
     fi
+
     if [ ! -d "${public_homedir}/files/${k}" ]; then
         exit 1
+    else
+        # Share the just created folders to the `users` group
+        # API ref: https://docs.nextcloud.com/server/latest/developer_manual/client_apis/OCS/ocs-share-api.html#create-a-new-share
+        curl -u ${public_username}:${public_password} -i -L -H "OCS-APIRequest: true" -X POST \
+            http://nextcloud-nginx/ocs/v1.php/apps/files_sharing/api/v1/shares \
+            -F "shareType=1" -F "path=/${k}" -F "shareWith=${users_groupname}"
     fi
 done
+
