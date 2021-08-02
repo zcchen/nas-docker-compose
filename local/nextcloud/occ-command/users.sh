368178e99f7c68c5d2f72afa4f41555765b59f64
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-08-02 10:34:04 +0800

    Added permissions setup for the shared folders.

--- local/nextcloud/occ-command/users.sh
+++ local/nextcloud/occ-command/users.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 occ_cmd="php /var/www/html/occ"
+ocs_api_base="http://nextcloud-nginx/ocs/v1.php"
 
 public_username=public
 public_password=$(cat /run/secrets/nextcloud-public-password)
@@ -32,9 +33,10 @@ for k in "Public" "Downloads" "Movies" "Pictures" "Softwares"; do
     else
         # Share the just created folders to the `users` group
         # API ref: https://docs.nextcloud.com/server/latest/developer_manual/client_apis/OCS/ocs-share-api.html#create-a-new-share
-        curl -u ${public_username}:${public_password} -i -L -H "OCS-APIRequest: true" -X POST \
-            http://nextcloud-nginx/ocs/v1.php/apps/files_sharing/api/v1/shares \
-            -F "shareType=1" -F "path=/${k}" -F "shareWith=${users_groupname}"
+        curl -u ${public_username}:${public_password} -i -L -H "OCS-APIRequest: true" \
+            -X POST ${ocs_api_base}/apps/files_sharing/api/v1/shares \
+            -F "path=/${k}" -F "shareType=1" -F "shareWith=${users_groupname}" \
+            -F "permissions=15"
     fi
 done
 
