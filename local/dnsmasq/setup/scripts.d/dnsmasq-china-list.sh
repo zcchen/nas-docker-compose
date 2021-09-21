e1820143d06d7947cceb9d79cadfd43ca5158d1f
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-09-21 22:07:48 +0800

    Fixed the dnsmasq reboot failed error.

--- local/dnsmasq/setup/scripts.d/dnsmasq-china-list.sh
+++ local/dnsmasq/setup/scripts.d/dnsmasq-china-list.sh
@@ -40,18 +40,5 @@ for _server in "${SERVERS[@]}"; do
   sed -i "s|^\(server.*\)/[^/]*$|\1/$_server|" /etc/dnsmasq.d/*."$_server".conf
 done
 
-echo "Restarting dnsmasq service..."
-if hash systemctl 2>/dev/null; then
-  systemctl restart dnsmasq
-elif hash service 2>/dev/null; then
-  service dnsmasq restart
-elif hash rc-service 2>/dev/null; then
-  rc-service dnsmasq restart
-elif hash busybox 2>/dev/null && [[ -d "/etc/init.d" ]]; then
-  /etc/init.d/dnsmasq restart
-else
-  echo "Now please restart dnsmasq since I don't know how to do it."
-fi
-
 echo "Cleaning up..."
 rm -r "$WORKDIR"
