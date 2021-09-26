977b63aacc55f772d7d4c335a97233fc9f6fa21b
zcchen <Bill.Chen.fs.cn@outlook.com>
2021-09-27 00:13:01 +0800

    Add Music for samba folders.

--- local/nextcloud/occ-command/files.sh
+++ local/nextcloud/occ-command/files.sh
@@ -5,7 +5,7 @@ mnt_dir="/mnt"
 
 ${occ_cmd} app:enable -f files_external
 
-for d in Public Downloads Videos Pictures Softwares; do
+for d in Public Downloads Videos Pictures Softwares Music; do
   if [ -z "$(${occ_cmd} files_external:list | grep ${d})" ]; then
     echo ">>> seting up the mount point <$d>..."
     mount_id=$(${occ_cmd} files_external:create \
